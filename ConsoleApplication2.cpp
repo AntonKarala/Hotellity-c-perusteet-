522ea36a4543152692e86ddc024c8fbc1707dfad
AntonKarala <antonkarala10@gmail.com>
2024-12-11 16:58:52 +0200

    Update ConsoleApplication2.cpp

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,5 +1,5 @@
-//Harjoitustyˆ Anton Karala 24Tietoa 
-// Hotelli varausj‰rjestelm‰ (3p)
+//Harjoitusty√∂ Anton Karala 24Tietoa 
+// Hotelli varausj√§rjestelm√§ (3p)
 
 
 // Tarvittavat kirjastot                
@@ -12,11 +12,11 @@
 
 using namespace std;
 
-// M‰‰ritell‰‰n huoneiden m‰‰r‰t vakiona
+// M√§√§ritell√§√§n huoneiden m√§√§r√§t vakiona
 const int MIN_HUONEET = 30;
 const int MAX_HUONEET = 300;
 
-// Sama huoneen hinnalle v‰lill‰ 80e-100e
+// Sama huoneen hinnalle v√§lill√§ 80e-100e
 const int MIN_HINTA = 80;
 const int MAX_HINTA = 100;
 
@@ -39,13 +39,13 @@ void teeVaraus(vector<bool>& huoneet, map<int, Varaus>& varaukset, int& varattuj
 void haeVaraus(const map<int, Varaus>& varaukset);
 bool tarkistaSyote(string syote);
 
-// P‰‰ohjelma
+// P√§√§ohjelma
 int main() {
-    //ƒ‰kkˆsten tulostus 
+    //√Ñ√§kk√∂sten tulostus 
     setlocale(LC_ALL,"fi_FI");
     srand(time(0)); // Satunnaislukugeneraattori
 
-    // huoneiden m‰‰rien ja hintojen arvonta 
+    // huoneiden m√§√§rien ja hintojen arvonta 
     int huonemaara = arvoLuku(MIN_HUONEET, MAX_HUONEET);
     int hintaPerYo = arvoLuku(MIN_HINTA, MAX_HINTA);
 
@@ -56,24 +56,27 @@ int main() {
     int varattujaHuoneita = 0; // laskur varatuille huoneille
     string valinta;
 
-    cout << "Hotellin varausjarjestelm‰ " << endl;
-    cout << "Hotellissa on yhteens‰ " << huonemaara << " huonetta." << endl;
-    cout << "Hinta t‰n‰ yˆna on: " << hintaPerYo << " euroa." << endl;
+    //menun tulostus
 
-    // P‰‰silmukka
+    cout << "Hotellin varausjarjestelm√§ " << endl;
+    cout << "Hotellissa on yhteens√§ " << huonemaara << " huonetta." << endl;
+    cout << "Hinta t√§n√§ y√∂na on: " << hintaPerYo << " euroa." << endl;
+
+    // P√§√§silmukka
     while (true) {
         naytaValikko();
         cout << "Valintasi: ";
         cin >> valinta;
 
-        // Tarkistetaan syˆte
+        // Tarkistetaan sy√∂te
         if (!tarkistaSyote(valinta)) {
-            cout << "Virheellinen syˆte, yrit‰ uudelleen." << endl;
+            cout << "Virheellinen sy√∂te, yrit√§ uudelleen." << endl;
             continue;
         }
 
         int valintaNum = stoi(valinta);
-
+        
+        // eri vaihtoehdot k√§ytt√§j√§n sy√∂tteelle
         if (valintaNum == 1) {
             teeVaraus(huoneet, varaukset, varattujaHuoneita);
         }
@@ -92,7 +95,7 @@ int main() {
     return 0;
 }
 
-// K‰ytet‰‰n simppeli‰ aliohjelmaa satunnaislukujen arvontaan
+// K√§ytet√§√§n simppeli√§ aliohjelmaa satunnaislukujen arvontaan
 int arvoLuku(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
@@ -125,14 +128,14 @@ void teeVaraus(vector<bool>& huoneet, map<int, Varaus>& varaukset, int& varattuj
         cin >> syote;
 
         if (!tarkistaSyote(syote)) {
-            cout << "Virheellinen syˆte. Anna numero v‰lilt‰ 1-" << huoneet.size() << "." << endl;
+            cout << "Virheellinen sy√∂te. Anna numero v√§lilt√§ 1-" << huoneet.size() << "." << endl;
             continue;
         }
 
         huonenumero = stoi(syote);
 
         if (huonenumero < 1 || huonenumero > huoneet.size()) {
-            cout << "Huonenumero ei ole v‰lilt‰ 1-" << huoneet.size() << ". Kokeile uudelleen." << endl;
+            cout << "Huonenumero ei ole v√§lilt√§ 1-" << huoneet.size() << ". Kokeile uudelleen." << endl;
             continue;
         }
 
@@ -144,7 +147,7 @@ void teeVaraus(vector<bool>& huoneet, map<int, Varaus>& varaukset, int& varattuj
         }
     }
 
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Tyhjennet‰‰n syˆtteen puskuri
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Tyhjennet√§√§n sy√∂tteen puskuri
     cout << "Anna varaajan nimi: ";
     string nimi;
     getline(cin, nimi);
@@ -163,7 +166,7 @@ void teeVaraus(vector<bool>& huoneet, map<int, Varaus>& varaukset, int& varattuj
 
 // Aliohjelma varauksen hakemiseen
 void haeVaraus(const map<int, Varaus>& varaukset) {
-    cout << "Hae varausta varausnumerolla tai nimell‰." << endl;
+    cout << "Hae varausta varausnumerolla tai nimell√§." << endl;
     cout << "1. Varausnumero\n2. Nimi\nValintasi: ";
     string valinta;
     cin >> valinta;
@@ -181,22 +184,22 @@ void haeVaraus(const map<int, Varaus>& varaukset) {
         int varausnumero = stoi(syote);                 //Haku toiminto varausnumerolla
         auto it = varaukset.find(varausnumero);
         if (it != varaukset.end()) {
-            cout << "Varaus lˆytyi! Nimi: " << it->second.nimi << ", Huone: " << it->second.huonenumero << endl;
+            cout << "Varaus l√∂ytyi! Nimi: " << it->second.nimi << ", Huone: " << it->second.huonenumero << endl;
         }
         else {
-            cout << "Varausta ei lˆytynyt." << endl;
+            cout << "Varausta ei l√∂ytynyt." << endl;
         }
     }
-    else if (valinta == "2") {                //Haku nimell‰
+    else if (valinta == "2") {                //Haku nimell√§
         cout << "Anna varaajan nimi: ";
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Tyhjennet‰‰n syˆtteen puskuri
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Tyhjennet√§√§n sy√∂tteen puskuri
         string nimi;
         getline(cin, nimi);
 
         bool loytyi = false;
         for (const auto& pari : varaukset) {
             if (pari.second.nimi == nimi) {
-                cout << "Varaus lˆytyi! Varausnumero: " << pari.second.varausnumero << ", Huone: " << pari.second.huonenumero << endl;
+                cout << "Varaus l√∂ytyi! Varausnumero: " << pari.second.varausnumero << ", Huone: " << pari.second.huonenumero << endl;
                 loytyi = true;
                 break;
             }
@@ -211,7 +214,7 @@ void haeVaraus(const map<int, Varaus>& varaukset) {
     }
 }
 
-// Aliohjelma syˆtteen tarkistukseen
+// Aliohjelma sy√∂tteen tarkistukseen
 bool tarkistaSyote(string syote) {
     for (char c : syote) {
         if (!isdigit(c)) return false;
